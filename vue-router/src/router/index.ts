c78532b2cf9992eea3fbbf5734ac9f7a9e50b861
llHoYall <hoya128@gmail.com>
2021-06-18 23:38:27 +0900

    Added Project: Vue-Router

---
+++ vue-router/src/router/index.ts
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/hello",
+    name: "Hello",
+    component: () =>
+      import(
+        /* webpackChunkName: "hello", webpackPrefetch: true */ "../views/Hello.vue"
+      ),
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ vue-router/src/views/Hello.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <h1>Hello!</h1>
+    <h2>This is an example route page.</h2>
+  </div>
+</template>
